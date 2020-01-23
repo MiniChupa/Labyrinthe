837492e5153ef1122366d5633d7edb785dc03d0b
Karine <houbrix.karine@gmail.com>
2020-01-23 22:47:59 +0100

    Sortie + animation

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            GetComponent<Animator>().enabled = true;
+        }
+    }
+}
