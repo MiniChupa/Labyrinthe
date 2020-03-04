9a2dc21edac6d3f638c5210deaf067a3bb683afb
Karine <houbrix.karine@gmail.com>
2020-03-04 23:27:12 +0100

    Début de l'ui + compte à rebours

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -6,11 +6,25 @@ public class DoorScript : MonoBehaviour
 {
     public bool canOpen = false;
 
+    [SerializeField]
+    AudioClip soundOpen, soundCantOpen;
+
+    private AudioSource myAudioSource;
+
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(canOpen)
+        if (canOpen)
         {
             GetComponent<Animator>().enabled = true;
+            myAudioSource.PlayOneShot(soundOpen);
+        } else
+        {
+            myAudioSource.PlayOneShot(soundCantOpen);
         }
     }
 }
