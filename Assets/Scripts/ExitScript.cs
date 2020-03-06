adb7b023ad5530b78c048378bfa216d69075963f
MiniChupa <houbrix.karine@gmail.com>
2020-03-06 15:52:59 +0100

    Gestion des scènes

--- Assets/Scripts/ExitScript.cs
+++ Assets/Scripts/ExitScript.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitScript : MonoBehaviour
 {
+    [SerializeField]
+    int levelToLoad;
+
+    [SerializeField]
+    bool autoindex = true;
+
+    private void Start()
+    {
+        if(autoindex)
+        {
+            levelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+        }
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.collider.tag == "Player")
         {
-            Debug.Log("Vous êtes à la sortie bravo !");
+            SceneManager.LoadScene(levelToLoad);
         }
     }
 }
